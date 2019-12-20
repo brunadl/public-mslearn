dbc1eda90b46fb1eaf927959f442c8ee553b963f
Bruna Lanzarini <brunadolan@hotmail.com>
2019-12-20 19:17:41 -0300

    Testar mensagem do console móvel

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-console.log('Here\'s a hidden meesage');
-
 let today = new Date();
 let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+
+console.log('Here\'s a hidden meesage');
